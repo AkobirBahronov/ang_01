3cb3054669315adfdc9c1535383e28a98aec8d19
Akobir <akobirbahronov07@gmail.com>
2022-10-17 17:51:48 +0500

    10/17/22 5

--- src/app/my-comp/my-comp.component.ts
+++ src/app/my-comp/my-comp.component.ts
@@ -1,17 +1,93 @@
 import { Component } from '@angular/core';
-import { Service, Name } from './my-comp.service';
-
+import ArrayStore from 'devextreme/data/array_store';
+import DataSource from 'devextreme/data/data_source';
 @Component({
   selector: 'my-comp',
   templateUrl: './my-comp.component.html',
   styleUrls: ['./my-comp.component.css'],
-  providers: [Service],
 })
 export class MyCompComponent {
-  dataSource: Name[];
-
-  constructor(service: Service) {
-    this.dataSource = service.getData();
+  store: ArrayStore;
+  dataSource: DataSource;
+  names = [
+    {
+      ID: 1,
+      FirstName: 'John',
+      LastName: 'Heart',
+    },
+    {
+      ID: 2,
+      FirstName: 'Olivia',
+      LastName: 'Peyton',
+    },
+    {
+      ID: 3,
+      FirstName: 'Robert',
+      LastName: 'Reagan',
+    },
+    {
+      ID: 4,
+      FirstName: 'Greta',
+      LastName: 'Sims',
+    },
+    {
+      ID: 5,
+      FirstName: 'Brett',
+      LastName: 'Wade',
+    },
+    {
+      ID: 6,
+      FirstName: 'Sandra',
+      LastName: 'Johnson',
+    },
+    {
+      ID: 7,
+      FirstName: 'Kevin',
+      LastName: 'Carter',
+    },
+    {
+      ID: 8,
+      FirstName: 'Cynthia',
+      LastName: 'Stanwick',
+    },
+    {
+      ID: 9,
+      FirstName: 'Kent',
+      LastName: 'Samuelson',
+    },
+    {
+      ID: 10,
+      FirstName: 'Taylor',
+      LastName: 'Riley',
+    },
+    {
+      ID: 11,
+      FirstName: 'Sam',
+      LastName: 'Hill',
+    },
+    {
+      ID: 12,
+      FirstName: 'Kelly',
+      LastName: 'Rodriguez',
+    },
+    {
+      ID: 13,
+      FirstName: 'Natalie',
+      LastName: 'Maguirre',
+    },
+    {
+      ID: 14,
+      FirstName: 'Walter',
+      LastName: 'Hobbs',
+    },
+  ];
+  constructor() {
+    this.dataSource = new DataSource({
+      store: new ArrayStore({
+        key: 'ID',
+        data: this.names,
+      }),
+    });
     this.validationCheck = this.validationCheck.bind(this);
   }
 
